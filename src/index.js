c15b2e063da35f3ec67f55e394bcef22cb93d31c
Jeferson Farias Calazans <calazans10@gmail.com>
2017-12-10 14:56:44 -0200

    Extract dispatch object into an action creator

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './components/App';
 import store from './store';
 import { getReadableStories } from './selectors/story';
-import { STORY_ARCHIVE } from './constants/actionTypes';
+import { doArchiveStory } from './actions/archive';
 import registerServiceWorker from './registerServiceWorker';
 
 function render() {
   ReactDOM.render(
     <App
       stories={getReadableStories(store.getState())}
-      onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+      onArchive={id => store.dispatch(doArchiveStory(id))}
     />,
     document.getElementById('root')
   );
