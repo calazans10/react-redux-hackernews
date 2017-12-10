8384f791e1b86f593240b17d73508ad3078359ae
Jeferson Farias Calazans <calazans10@gmail.com>
2017-12-10 11:50:47 -0200

    Dispatch on archive action

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import store from './store';
+import { STORY_ARCHIVE } from './constants/actionTypes';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App stories={store.getState().storyState} onArchive={() => {}} />,
+  <App
+    stories={store.getState().storyState}
+    onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+  />,
   document.getElementById('root')
 );
 registerServiceWorker();
